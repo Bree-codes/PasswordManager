4eeed2a96d126dc57fe957823a4db4bb78b1a99b
Bree-codes <mwongera.mukami22@students.dkut.ac.ke>
2024-05-14 19:33:44 +0300

    working on registration API

---
+++ PasswordManagerWebApplication/src/main/java/com/bughunters/code/passwordmanagerwebapplication/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.bughunters.code.passwordmanagerwebapplication.repository;
+
+import com.bughunters.code.passwordmanagerwebapplication.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+}
