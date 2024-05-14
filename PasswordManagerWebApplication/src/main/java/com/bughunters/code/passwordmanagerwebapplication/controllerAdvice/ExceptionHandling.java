2f8fe15d4938e33f42ac664ecf1746c303f5f91d
Bree-codes <mwongera.mukami22@students.dkut.ac.ke>
2024-05-14 23:47:27 +0300

    working on registration API

---
+++ PasswordManagerWebApplication/src/main/java/com/bughunters/code/passwordmanagerwebapplication/controllerAdvice/ExceptionHandling.java
@@ -0,0 +1,33 @@
+package com.bughunters.code.passwordmanagerwebapplication.controllerAdvice;
+
+import com.bughunters.code.passwordmanagerwebapplication.exceptions.UserAlreadyExistException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.Date;
+
+@ControllerAdvice
+@Slf4j
+public class ExceptionHandling {
+
+    private final ExceptionModel exceptionModel;
+    @Autowired
+    public ExceptionHandling(ExceptionModel exceptionModel) {
+        this.exceptionModel = exceptionModel;
+    }
+
+    @ExceptionHandler(UserAlreadyExistException.class)
+    public ResponseEntity<ExceptionModel> handleUserAlreadyExistException(UserAlreadyExistException exception){
+        log.error("Email Already ExistException occurred ");
+
+        exceptionModel.setStatus(HttpStatus.FORBIDDEN);
+        exceptionModel.setMessage(exception.getMessage());
+        exceptionModel.setDate(new Date());
+
+        return  new ResponseEntity<>(exceptionModel,HttpStatus.FORBIDDEN);
+    }
+}
