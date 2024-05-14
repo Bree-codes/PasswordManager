7b54c16bd47c3dbcea94492f08fd2079a5e086d0
Bree-codes <mwongera.mukami22@students.dkut.ac.ke>
2024-05-14 16:21:44 +0300

    Initilaized the project

---
+++ PasswordManagerWebApplication/src/main/java/com/bughunters/code/passwordmanagerwebapplication/PasswordManagerWebApplication.java
@@ -0,0 +1,13 @@
+package com.bughunters.code.passwordmanagerwebapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PasswordManagerWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PasswordManagerWebApplication.class, args);
+    }
+
+}
---
+++ PasswordManagerWebApplication/src/test/java/com/bughunters/code/passwordmanagerwebapplication/PasswordManagerWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bughunters.code.passwordmanagerwebapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PasswordManagerWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
