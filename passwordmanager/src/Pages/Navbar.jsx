5d5e611160da2fd92d94aef43e1870e89541831c
Fai-chebby <chebetf034@gmail.com>
2024-05-15 23:54:27 +0300

    Responsive

--- passwordmanager/src/Pages/Navbar.jsx
+++ passwordmanager/src/Pages/Navbar.jsx
@@ -1,12 +1,12 @@
 import { Link } from "react-router-dom";
-
+import "./styling/Navbar.css"
 export const Navbar = () => {
     return (
-        <nav>
+        <nav className="nav">
             <ul>
                 <li><Link to="/">Home</Link></li>
-                <li><Link to="/SignUp">SignUp</Link></li>
-                <li><Link to="/Login">Login</Link></li>
+                <li className="sign"><Link to="/SignUp">SignUp</Link></li>
+                <li  className="login"><Link to="/Login">Login</Link></li>
             </ul>
         </nav>
     );
