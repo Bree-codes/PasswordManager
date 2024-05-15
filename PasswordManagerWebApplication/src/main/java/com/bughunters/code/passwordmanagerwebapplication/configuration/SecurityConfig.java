2171517de71cb640cd0c8238198749f4295b5198
Bree-codes <mwongera.mukami22@students.dkut.ac.ke>
2024-05-15 11:28:25 +0300

    Configuring security

--- PasswordManagerWebApplication/src/main/java/com/bughunters/code/passwordmanagerwebapplication/configuration/SecurityConfig.java
+++ PasswordManagerWebApplication/src/main/java/com/bughunters/code/passwordmanagerwebapplication/configuration/SecurityConfig.java
@@ -1,7 +1,16 @@
 package com.bughunters.code.passwordmanagerwebapplication.configuration;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.AbstractAuthenticationToken;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.AbstractConfiguredSecurityBuilder;
+import org.springframework.security.config.annotation.AbstractSecurityBuilder;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
@@ -11,8 +20,27 @@ import org.springframework.stereotype.Component;
 public class SecurityConfig {
 
     @Bean
+    public HttpSecurity securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+
+        httpSecurity.csrf(AbstractHttpConfigurer::disable)
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .formLogin(Customizer.withDefaults())
+                .httpBasic(Customizer.withDefaults())
+                .authorizeHttpRequests(req -> req.requestMatchers("/api/password-manager/auth/**"))
+                .build();
+
+        return httpSecurity;
+    }
+
+
+    @Bean
     public PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager() throws Exception {
+        return  new AuthenticationConfiguration().getAuthenticationManager();
     }
 
 }
--- PasswordManagerWebApplication/src/main/java/com/bughunters/code/passwordmanagerwebapplication/controller/AuthenticationController.java
+++ PasswordManagerWebApplication/src/main/java/com/bughunters/code/passwordmanagerwebapplication/controller/AuthenticationController.java
@@ -9,10 +9,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
-@RestController("/pwmanager")
+@RestController
+@RequestMapping("/api/password-manager/auth")
 public class AuthenticationController {
 
     private final AuthenticationService authenticationService;
