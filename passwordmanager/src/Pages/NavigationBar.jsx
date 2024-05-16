247cc992d281d2ddde137e13bd770f29b343e97b
Fai-chebby <chebetf034@gmail.com>
2024-05-16 13:15:43 +0300

    NavigationBar

--- passwordmanager/src/Pages/NavigationBar.jsx
+++ passwordmanager/src/Pages/NavigationBar.jsx
@@ -4,15 +4,24 @@ import "./styling/NavigationBar.css"
 
 export const NavigationBar = () => {
     return (
-            <Navbar expand="lg" className="bg-body-tertiary">
+            <Navbar expand="md" className="bg-body-tertiary">
                 <Container fluid>
-                    <Navbar.Toggle aria-controls="navbarScroll" />
+                    <Navbar.Toggle aria-controls="navbarScroll"/>
+                    <div className={"home-md"}>
+                        <div className={"home-icon"}><Link to="/">Home</Link></div>
+                        <div className={"links"}>
+                            <div className="sign"><Link to="/SignUp">SignUp</Link></div>
+                            <div className="login"><Link to="/Login">Login</Link></div>
+                        </div>
+                    </div>
                     <Navbar.Collapse id="navbarScroll">
-                        <Nav  className="nav" navbarScroll>
-                            <ul>
-                          <li>  <Link className="link" to="/">Home</Link> </li>
-                                <li><Link className="link" to="/SignUp">SignUp</Link> </li>
-                                <li><Link className="link" to="/Login">Login</Link></li>
+                        <Nav className="nav" navbarScroll>
+                            <ul className={"home"}>
+                                <li className={"home-icon"}><Link className="link" to="/">Home</Link></li>
+                                <ul className={"links"}>
+                                    <li className="sign"><Link to="/SignUp">SignUp</Link></li>
+                                    <li className="login"><Link to="/Login">Login</Link></li>
+                                </ul>
                             </ul>
                         </Nav>
                     </Navbar.Collapse>
