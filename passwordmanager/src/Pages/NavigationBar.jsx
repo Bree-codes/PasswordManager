1577298c2913e457aa1f0d4ae2547644cccb9b40
Fai-chebby <chebetf034@gmail.com>
2024-05-16 12:36:50 +0300

    NavigationBar

--- passwordmanager/src/Pages/NavigationBar.jsx
+++ passwordmanager/src/Pages/NavigationBar.jsx
@@ -1,5 +1,6 @@
 import {Link} from "react-router-dom";
 import {Container, Nav, Navbar} from "react-bootstrap";
+import "./styling/NavigationBar.css"
 
 export const NavigationBar = () => {
     return (
@@ -7,9 +8,12 @@ export const NavigationBar = () => {
                 <Container fluid>
                     <Navbar.Toggle aria-controls="navbarScroll" />
                     <Navbar.Collapse id="navbarScroll">
-                        <Nav navbarScroll>
-                            <Link to="/SignUp">SignUp</Link>
-                            <Link to="/Login">Login</Link>
+                        <Nav  className="nav" navbarScroll>
+                            <ul>
+                          <li>  <Link className="link" to="/">Home</Link> </li>
+                                <li><Link className="link" to="/SignUp">SignUp</Link> </li>
+                                <li><Link className="link" to="/Login">Login</Link></li>
+                            </ul>
                         </Nav>
                     </Navbar.Collapse>
                 </Container>
