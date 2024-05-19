0e8ea6bf9514ece5bad813f95e63e169c36aa524
Fai-chebby <chebetf034@gmail.com>
2024-05-19 23:56:16 +0300

    Home page

--- passwordmanager/src/App.js
+++ passwordmanager/src/App.js
@@ -12,7 +12,7 @@ function App() {
             <Route path={"/"}  element={(<RefreshAuthentication><NavigationBar /></RefreshAuthentication>)}>
                 <Route path="SignUp" element={<SignUp/>}/>
                 <Route path="Login" element={<Login/>}/>
-                <Route path={"Home"} element={Home}>
+                <Route path={"Home"} element={<Home/>}
                     <Route path={"view/passwords"} element={<h1>password view page.</h1>} />
                 </Route>
                 <Route path={"*"} element={<h1>Page Not Found</h1>} />
--- passwordmanager/src/Pages/Home.jsx
+++ passwordmanager/src/Pages/Home.jsx
@@ -3,12 +3,16 @@ export const Home =()=>{
     return(
         <div>
         <div>
+            <h1> Say goodbye to forgetting password</h1>
             <h2>
                 <span> Make life</span>
                 <span> simple & safe</span>
 
             </h2>
         </div>
+            <div>
+
+            </div>
 
         </div>
     )
