1c630382ea025505194b76700a065fec066120b0
Fai-chebby <chebetf034@gmail.com>
2024-05-19 23:44:19 +0300

    Home page

--- passwordmanager/src/Pages/Home.jsx
+++ passwordmanager/src/Pages/Home.jsx
@@ -2,7 +2,13 @@
 export const Home =()=>{
     return(
         <div>
+        <div>
+            <h2>
+                <span> Make life</span>
+                <span> simple & safe</span>
 
+            </h2>
+        </div>
 
         </div>
     )
