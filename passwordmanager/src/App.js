dea525dcd57de3f7a53b84df5ca66b9c833a3d46
Red-stevo <stephen.muiru22@students.dkut.ac.ke>
2024-05-19 14:11:02 +0300

    Commit message >>>>> configuring auto authentication when refresh token is available <<<<<

--- passwordmanager/src/App.js
+++ passwordmanager/src/App.js
@@ -2,21 +2,22 @@ import {Route, Routes} from "react-router-dom";
 import {SignUp} from "./Pages/SignUp";
 import {Login} from "./Pages/Login";
 import {NavigationBar} from "./Pages/NavigationBar";
+import RefreshAuthentication from "./Pages/styling/RefreshAuthentication";
+
 function App() {
   return (
     <div className="App">
         <Routes>
-            <Route path={"/"}  element={<NavigationBar />}>
+            <Route path={"/"}  element={(<RefreshAuthentication><NavigationBar /></RefreshAuthentication>)}>
                 <Route path="SignUp" element={<SignUp/>}/>
                 <Route path="Login" element={<Login/>}/>
-                <Route path={"*"} element={<h1>Page Not Found</h1>} />
-            </Route>
             <Route path={"home"} element={<h1>Home page</h1>}>
-            <Route path={"view/passwords"} element={<h1>password view page.</h1>} />
+                <Route path={"view/passwords"} element={<h1>password view page.</h1>} />
+            </Route>
+            <Route path={"*"} element={<h1>Page Not Found</h1>} />
             </Route>
         </Routes>
-    </div>
-  );
+    </div>);
 }
 
 export default App;
