1f91a9f843974633fab4aef26f207e2206b0a2f0
Red-stevo <stephen.muiru22@students.dkut.ac.ke>
2024-05-20 09:43:13 +0300

    Commit message >>>>> configured protected routes <<<<<

--- passwordmanager/src/App.js
+++ passwordmanager/src/App.js
@@ -3,6 +3,7 @@ import {SignUp} from "./Pages/SignUp";
 import {Login} from "./Pages/Login";
 import {NavigationBar} from "./Pages/NavigationBar";
 import RefreshAuthentication from "./Pages/styling/RefreshAuthentication";
+import ProtectedRoutes from "./Pages/Components/ProtectedRoutes";
 
 function App() {
   return (
@@ -12,7 +13,7 @@ function App() {
                 <Route path="SignUp" element={<SignUp/>}/>
                 <Route path="Login" element={<Login/>}/>
             </Route>
-            <Route path={"home"} element={<><h1>Home page Nav bar</h1><Outlet/></>}>
+            <Route path={"home"} element={<ProtectedRoutes><><h1>Home page Nav bar</h1><Outlet/></></ProtectedRoutes>}>
                 <Route path={"view/passwords"} element={<h2>password view page.</h2>} />
             </Route>
             <Route path={"*"} element={<h1>Page Not Found</h1>} />
