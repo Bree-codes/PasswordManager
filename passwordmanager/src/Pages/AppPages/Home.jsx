c9f453cbdd008f6d8e1010acecda73f678cfc4b3
Fai-chebby <chebetf034@gmail.com>
2024-05-20 19:07:16 +0300

    Home design

--- passwordmanager/src/Pages/AppPages/Home.jsx
+++ passwordmanager/src/Pages/AppPages/Home.jsx
@@ -1,5 +1,5 @@
 import {Outlet} from "react-router-dom";
-
+import secure from "../pics/secure.jpeg"
 export const Home =()=>{
     return(
         <>
@@ -12,7 +12,7 @@ export const Home =()=>{
                     </h2>
                 </div>
                 <div className="image">
-                    <img src="secure-image.jpg" alt="Secure Image" />
+                    <img src={secure}  alt={" Security pic"} />
                 </div>
             </div>
             );
