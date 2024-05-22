570f2e747f38b0461a00a98f5b0235c3481d8cf8
Fai-chebby <chebetf034@gmail.com>
2024-05-22 13:28:24 +0300

    Profile page

--- passwordmanager/src/Pages/AppPages/Profile.jsx
+++ passwordmanager/src/Pages/AppPages/Profile.jsx
@@ -2,10 +2,17 @@
 export const profile=()=>{
     return(
         <div className="profile">
+            <div className="avatar">
+                <div className="avatar-wrapper">
+
+                </div>
+
+            </div>
+            <div className="profile name">
             <p>Name:</p>
             <p>Email:</p>
             <p>Account created at:</p>
-
+            </div>
         </div>
     )
 }
