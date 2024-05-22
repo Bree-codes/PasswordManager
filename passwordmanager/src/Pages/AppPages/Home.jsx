289d5f95fd4d74f3d859949bfd6f1f13effe9288
Fai-chebby <chebetf034@gmail.com>
2024-05-22 11:46:29 +0300

    Footer settings

--- passwordmanager/src/Pages/AppPages/Home.jsx
+++ passwordmanager/src/Pages/AppPages/Home.jsx
@@ -1,6 +1,7 @@
 import {Outlet} from "react-router-dom";
 import secure from "../pics/secure.jpeg"
 import "../styling/Home.css"
+import {Footer} from "./Footer";
 export const Home =()=>{
     return(
         <>
@@ -19,6 +20,7 @@ export const Home =()=>{
 
 
             <Outlet />
+            <Footer/>
         </>
     );
 }
