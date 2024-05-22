10c331b1508f760a6e1c739e10cef5fa11e6c4c7
Red-stevo <stephen.muiru22@students.dkut.ac.ke>
2024-05-22 18:36:28 +0300

    Commit message >>>>> designed basic page layout <<<<<

--- passwordmanager/src/Pages/Components/PasswordView.jsx
+++ passwordmanager/src/Pages/Components/PasswordView.jsx
@@ -1,6 +1,24 @@
-const PasswordView = () => {
-    return (<div className={"password-view"}>
+import {Form} from "react-bootstrap";
+import "./../styling/HomePage.css"
 
+const PasswordView = ({websiteName, username, password}) => {
+    return (<div className={"password-view"}>
+                <Form>
+                    <Form.Group>
+                        <Form.Label className={"website-name"} htmlFor={"website-name"}>Website</Form.Label>
+                        <Form.Control id="website-name" type={"text"} disabled={true} value={"website name"} />
+                    </Form.Group>
+
+                    <Form.Group>
+                        <Form.Label className={"username"} htmlFor={"Username"}>Username</Form.Label>
+                        <Form.Control id="Username" type={"text"} disabled={true} value={"username"} />
+                    </Form.Group>
+
+                    <Form.Group>
+                        <Form.Label className={"password"} htmlFor={"Password"}>Password</Form.Label>
+                        <Form.Control id="Password"  type={"text"} disabled={true} value={"website name"} />
+                    </Form.Group>
+                </Form>
             </div>);
 }
 
