72bc86eec9a3d2f9e69807aa463a7ffe625a742a
Red-stevo <stephen.muiru22@students.dkut.ac.ke>
2024-05-22 19:14:45 +0300

    Commit message >>>>> designed the eye show and hide <<<<<

--- passwordmanager/src/Pages/Components/PasswordView.jsx
+++ passwordmanager/src/Pages/Components/PasswordView.jsx
@@ -1,7 +1,13 @@
-import {Form} from "react-bootstrap";
+import {Button, Col, Form, Image} from "react-bootstrap";
 import "./../styling/HomePage.css"
+import eyeSlash from "./../pics/eye-slash-solid.svg"
+import eye from "./../pics/eye-solid.svg"
+import {useState} from "react";
 
 const PasswordView = ({websiteName, username, password, doEdit}) => {
+    const [see, setSee] = useState(false);
+
+
     return (<div className={"password-view"}>
                 <Form className={"details-view-form"}>
                     <Form.Group>
@@ -16,7 +22,13 @@ const PasswordView = ({websiteName, username, password, doEdit}) => {
 
                     <Form.Group>
                         <Form.Label className={"password"} htmlFor={"Password"}>Password</Form.Label>
-                        <Form.Control id="Password"  type={"text"} disabled={true} value={"website name"} />
+                        <div id={"password-view"}>
+                            <Form.Control id="Password"  type={"text"} disabled={true} value={"website name"} />
+                            {see ? <Button onClick={() => setSee(false)}
+                                           id={"view"}><Image src={eye} width={20} height={20} /></Button> :
+                            <Button onClick={() => setSee(true)}
+                                    id={"view"}><Image src={eyeSlash} width={20} height={20} /></Button>}
+                        </div>
                     </Form.Group>
                 </Form>
             </div>);
